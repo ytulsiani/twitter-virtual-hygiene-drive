c1e15a664dd7df83f63f6862b67cd3561d248294
StAnthonySF <89093148+StAnthonySF@users.noreply.github.com>
2021-08-30 10:14:51 -0700

    Update itemList.js

--- js/itemList.js
+++ js/itemList.js
@@ -5,7 +5,7 @@ var itemList = [
     "itemName": "Hangers",
     "servingUnits": "set of 10",
     "ourPrice": 8,
-    "retailPrice": 20,
+    "retailPrice": 8,
     "unitsReceipt": "set",
     "nameReceipt": "Hangers"
   },
@@ -15,19 +15,19 @@ var itemList = [
     "itemName": "Dryer Sheets",
     "servingUnits": "case of (20)",
     "ourPrice": 11,
-    "retailPrice": 20,
+    "retailPrice": 11,
     "unitsReceipt": "case",
     "nameReceipt": "Dryer Sheets"
   },
   {
     "quantity": 0, 
-    "image": "img/soap.jpg",
-    "itemName": "Soap",
+    "image": "img/shampoo.jpg",
+    "itemName": "Shampoo/Soap",
     "servingUnits": "case of (72)",
     "ourPrice": 16,
-    "retailPrice": 120,
+    "retailPrice": 16,
     "unitsReceipt": "case",
-    "nameReceipt": "Soap"
+    "nameReceipt": "Shampoo/Soap"
   },
   {
     "quantity": 0, 
@@ -35,89 +35,89 @@ var itemList = [
     "itemName": "Hygiene Kit",
     "servingUnits": "case of (150)",
     "ourPrice": 19,
-    "retailPrice": 120,
+    "retailPrice": 19,
     "unitsReceipt": "case",
     "nameReceipt": "Hygiene Kit"
   },
   {
     "quantity": 0, 
     "image": "img/wipes.jpg",
-    "itemName": "Wipes",
+    "itemName": "Laundry Detergent",
     "servingUnits": "case of (200)",
-    "ourPrice": 30,
-    "retailPrice": 120,
+    "ourPrice": 22,
+    "retailPrice": 22,
     "unitsReceipt": "case",
-    "nameReceipt": "Wipes"
+    "nameReceipt": "Laundry Detergent"
   },
   {
     "quantity": 0, 
     "image": "img/chapstick.jpg",
-    "itemName": "Chapstick",
+    "itemName": "Underwear/Socks",
     "servingUnits": "case of (50)",
-    "ourPrice": 35,
-    "retailPrice": 120,
+    "ourPrice": 28,
+    "retailPrice": 28,
     "unitsReceipt": "case",
-    "nameReceipt": "Chapstick"
+    "nameReceipt": "Underwear/Socks"
   },
   {
     "quantity": 0, 
     "image": "img/toothpaste.jpg",
-    "itemName": "Toothpaste",
+    "itemName": "Shower Robe/Cap",
     "servingUnits": "case of (288)",
-    "ourPrice": 45,
-    "retailPrice": 120,
+    "ourPrice": 36,
+    "retailPrice": 36,
     "unitsReceipt": "case",
-    "nameReceipt": "Toothpaste"
+    "nameReceipt": "Shower Robe/Cap"
   },
    {
-    "quantity": 0, 
-    "image": "img/shampoo.jpg",
-    "itemName": "Shampoo",
-    "servingUnits": "case of (288)",
-    "ourPrice": 50,
-    "retailPrice": 120,
-    "unitsReceipt": "case",
-    "nameReceipt": "Shampoo"
-  },
-   {
-    "quantity": 0, 
-    "image": "img/deodorant.png",
-    "itemName": "Deodorant",
-    "servingUnits": "case of (192)",
-    "ourPrice": 60,
-    "retailPrice": 120,
-    "unitsReceipt": "case",
-    "nameReceipt": "Deodorant"
-  },
-  {
     "quantity": 0, 
     "image": "img/towels.jpg",
     "itemName": "Towels",
-    "servingUnits": "case of (36)",
-    "ourPrice": 75,
-    "retailPrice": 120,
+    "servingUnits": "case of (288)",
+    "ourPrice": 44,
+    "retailPrice": 44,
     "unitsReceipt": "case",
     "nameReceipt": "Towels"
+  },
+   {
+    "quantity": 0, 
+    "image": "img/clothes.png",
+    "itemName": "Men's Clothing",
+    "servingUnits": "One Men's Outfit",
+    "ourPrice": 54,
+    "retailPrice": 54,
+    "unitsReceipt": "case",
+    "nameReceipt": "Men's Clothing"
+  },
+  {
+    "quantity": 0, 
+    "image": "img/clothing.jpg",
+    "itemName": "Women's Clothing",
+    "servingUnits": "case of (36)",
+    "ourPrice": 58,
+    "retailPrice": 58,
+    "unitsReceipt": "case",
+    "nameReceipt": "Women's Clothing"
   },
     {
     "quantity": 0, 
     "image": "img/razor.jpg",
-    "itemName": "Razors",
-    "servingUnits": "case of (1000)",
-    "ourPrice": 100,
-    "retailPrice": 120,
+    "itemName": "Champion of The Hygiene Hub",
+    "servingUnits": "single",
+    "ourPrice": 300,
+    "retailPrice": 300,
     "unitsReceipt": "case",
-    "nameReceipt": "Razors"
+    "nameReceipt": "Champion ofthe Hygiene Hub"
   },
     {
     "quantity": 0, 
     "image": "img/clothes.png",
-    "itemName": "Clothing Kit",
+    "itemName": "Super Patron",
     "servingUnits": "one set",
-    "ourPrice": 40,
-    "retailPrice": 120,
+    "ourPrice": 500,
+    "retailPrice": 500,
     "unitsReceipt": "set",
-    "nameReceipt": "Clothing"
+    "nameReceipt": "Super Patron"
   }
 ]
 
